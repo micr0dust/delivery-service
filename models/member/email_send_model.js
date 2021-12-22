303ca837f6c8cfa81f6093f6fafd7ac020c2838e
microdust <wuilliamolgo104286@gmail.com>
2021-12-22 08:47:24 +0800

    gmail

--- models/member/email_send_model.js
+++ models/member/email_send_model.js
@@ -31,31 +31,31 @@ module.exports = async function mailEmit(id, time) {
         // 寄驗證信
         try {
             //OAuth2 驗證
-            // const oauth2Client = new OAuth2(
-            //     config.mail.id,
-            //     config.mail.secret, // Client Secret
-            //     "https://developers.google.com/oauthplayground" // Redirect URL
-            // );
-            // oauth2Client.setCredentials({
-            //     refresh_token: config.mail.freshToken
-            // });
-            // const accessToken = oauth2Client.getAccessToken();
-            // const transporter = nodemailer.createTransport({
-            //     host: 'smtp.gmail.com',
-            //     port: 465,
-            //     secure: true,
-            //     auth: {
-            //         type: "OAuth2",
-            //         user: config.mail.account,
-            //         clientId: config.mail.id,
-            //         clientSecret: config.mail.secret,
-            //         refreshToken: config.mail.freshToken,
-            //         accessToken: accessToken
-            //     },
-            //     tls: {
-            //         rejectUnauthorized: false
-            //     }
-            // });
+            const oauth2Client = new OAuth2(
+                config.mail.id,
+                config.mail.secret, // Client Secret
+                "https://developers.google.com/oauthplayground" // Redirect URL
+            );
+            oauth2Client.setCredentials({
+                refresh_token: config.mail.freshToken
+            });
+            const accessToken = oauth2Client.getAccessToken();
+            const transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true,
+                auth: {
+                    type: "OAuth2",
+                    user: config.mail.account,
+                    clientId: config.mail.id,
+                    clientSecret: config.mail.secret,
+                    refreshToken: config.mail.freshToken,
+                    accessToken: accessToken
+                },
+                tls: {
+                    rejectUnauthorized: false
+                }
+            });
             //低安全性驗證
             // console.log(config.mail.account, config.mail.password);
             // var transporter = nodemailer.createTransport({
@@ -69,22 +69,23 @@ module.exports = async function mailEmit(id, time) {
             //     }
             // });
             //nodemailer SMTP 伺服器測試
-            var transporter = nodemailer.createTransport({
-                host: "smtp.mailtrap.io",
-                port: 2525,
-                auth: {
-                    user: "c6acc18c2879f4",
-                    pass: "8ec458587916cd"
-                }
-            });
-            console.log(member.email, verityCode);
-            // send mail with defined transport object
-            const mailOptions = {
-                from: config.mail.account,
-                to: member.email,
-                subject: 'Hello中原外送平台-信箱驗證',
-                html: '<h1>' + verityCode + '</h1><p>使用者 ' + member.name + ' 您好，您的驗證碼為 ' + verityCode + '</p><p>驗證碼將在十分鐘後無效，如果你沒有註冊過本平台請無視此郵件</p>',
-            };
+            // var transporter = nodemailer.createTransport({
+            //     host: "smtp.mailtrap.io",
+            //     port: 2525,
+            //     auth: {
+            //         user: "c6acc18c2879f4",
+            //         pass: "8ec458587916cd"
+            //     }
+            // });
+            // console.log(member.email, verityCode);
+            // // send mail with defined transport object
+            // const mailOptions = {
+            //     from: config.mail.account,
+            //     to: member.email,
+            //     subject: 'Hello中原外送平台-信箱驗證',
+            //     html: '<h1>' + verityCode + '</h1><p>使用者 ' + member.name + ' 您好，您的驗證碼為 ' + verityCode + '</p><p>驗證碼將在十分鐘後無效，如果你沒有註冊過本平台請無視此郵件</p>',
+            // };
+
             transporter.sendMail(mailOptions, (error, response) => {
                 error ? console.log(error) : console.log(response);
                 transporter.close();
