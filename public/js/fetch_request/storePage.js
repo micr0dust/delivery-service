5cfcd34c54982c0c34268ab1579acddfd3d1188e
micr0dust <wuilliamolgo104286@gmail.com>
2022-04-24 23:34:25 +0800

    del product

--- public/js/fetch_request/storePage.js
+++ public/js/fetch_request/storePage.js
@@ -1,37 +1,3 @@
-window.addEventListener('hashchange', function() {
-    if (!location.hash.split('#')[1]) return;
-    let selected = document.getElementById(location.hash.split('#')[1]);
-    let name = selected.querySelector('a > div.card-footer.justify-content-between > span:nth-child(1)').textContent;
-    Swal.fire({
-        title: '刪除商品',
-        html: `在下方輸入 <span class="font-weight-bold text-danger">${name}</span> 以確認刪除`,
-        input: 'text',
-        inputAttributes: {
-            autocapitalize: 'off'
-        },
-        showCancelButton: true,
-        confirmButtonColor: '#bf0d0d',
-        cancelButtonColor: '#808080',
-        confirmButtonText: '刪除',
-        showLoaderOnConfirm: true,
-        preConfirm: (confirm) => {
-            if (confirm == name) return true;
-            else Swal.showValidationMessage(
-                '輸入錯誤'
-            )
-        },
-        allowOutsideClick: () => !Swal.isLoading()
-    }).then((result) => {
-        history.replaceState(null, null, ' ');
-        if (result.isConfirmed)
-            Swal.fire(
-                '刪除成功',
-                `${name} 已經被刪除`,
-                'success'
-            )
-    })
-}, false);
-
 async function getProductFn(url) {
     let headersList = {
         Accept: '*/*',
