2fa8b2459b14a7035b47a17b00afdd9d36ffb88f
micr0dust <wuilliamolgo104286@gmail.com>
2022-08-20 12:35:46 +0800

    api doc

--- swagger.js
+++ swagger.js
@@ -32,20 +32,15 @@ const doc = {
         redirect_url: { 'redirect_url': 'https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&redirect_uri=http://localhost:3000/member/google/callback&response_type=code&client_id=1047494292997-3ph1u638fcq835lnt2dgfmntj8.apps.googleusercontent.com' },
         token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbGdvcml0aG0iOiJIUzI1NiIsImV4cCI6MTY1MDEyMTc1MCwiZGF0YSI6IjYyNWFkMDE2NzI0MGNhMzgzOWZlYmQwNSIsImlhdCI6MTY1MDExOTk1MH0.zK_jiJATx4W1INvg1yJsWnn_cnXjD2oc38DaRVTTx8k",
         order: {
-            "order": "[{\"_id\":\"62e34a28167d90df4ef71efc\",\"name\":\"超大漢堡\",\"price\":159,\"type\":\"漢堡\",\"discount\":null,\"note\":\"\",\"options\":\"[{\\\"title\\\":\\\"肉種\\\",\\\"option\\\":\\\"牛肉\\\"},{\\\"title\\\":\\\"加料\\\",\\\"option\\\":[\\\"雙層肉\\\"]}]\",\"count\":1}]",
-            "total": 144,
-            "store": "8y3un9ka",
-            "store_info": {
-                "name": "友朋小吃",
-                "address": "台灣台北市中山區民權東路三段榮星花園"
-            },
-            "discount": "[\"滿100元，現省15元\"]",
-            "tableware": true
+            "id": "62e34a28167d90df4ef71efc",
+            "count": 1,
+            "note": "",
+            "options": "[{\"title\":\"肉種\",\"option\":\"牛肉\"},{\"title\":\"加料\",\"option\":[\"雙層肉\",\"2倍起司\"]}]"
         },
         orderStr: "[{\"_id\":\"62e34a28167d90df4ef71efc\",\"name\":\"超大漢堡\",\"price\":169,\"type\":\"漢堡\",\"discount\":null,\"note\":\"\",\"options\":\"[{\\\"title\\\":\\\"肉種\\\",\\\"option\\\":\\\"牛肉\\\"},{\\\"title\\\":\\\"加料\\\",\\\"option\\\":[\\\"雙層肉\\\",\\\"2倍起司\\\"]}]\"}]",
         discountStr: "[\"滿100元，現省15元\"]",
         discount: '[{"method":"exceedPriceDiscount","goal":100,"discount":15}]',
-        options: '[{ "title": "肉種", "multiple": false, "requires": true, "option": [{ "name": "牛肉", "cost": "0" }, { "name": "雞肉", "cost": "0" }, { "name": "豬肉", "cost": "0" }, { "name": "魚肉", "cost": "0" }] }, { "title": "加料", "multiple": true, "requires": false, "option": [{ "name": "雙層肉", "cost": "20" }, { "name": "2倍起司", "cost": "10" }] }]'
+        options: '[{"_id":"62e34a28167d90df4ef71efc","name":"超大漢堡","price":169,"type":"漢堡","discount":null,"note":"","options":"[{\"title\":\"肉種\",\"option\":\"牛肉\"},{\"title\":\"加料\",\"option\":[\"雙層肉\",\"2倍起司\"]}]","count":1}]'
     }
 }
 
