fec9da1ade4424db6d1652218d520edc724df5a2
micr0dust <wuilliamolgo104286@gmail.com>
2023-03-08 17:03:49 +0800

    yarn upgrade

--- service/mongodbFns.js
+++ service/mongodbFns.js
@@ -1,3 +1,6 @@
+async function delay(ms) {
+    return await new Promise(resolve => setTimeout(resolve, ms));
+}
 module.exports = {
     findToArray: async (collection, filter)=> {
         return await collection.find(filter).toArray();
